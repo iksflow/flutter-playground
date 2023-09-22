29b4705c65e3f6d9e5705126e27b412efe9eeb43
iksflow <theiksflow@gmail.com>
2023-09-22 12:58:12 +0900

    :sparkles: Create Roll dice app.

---
+++ lib/udemy_roll_dice_app/dice_roller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: EdgeInsets.all(8.0),
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
