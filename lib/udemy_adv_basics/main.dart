84f509624ba53d30448154a666adf1ba6540b329
iksflow <theiksflow@gmail.com>
2023-10-05 23:06:27 +0900

    :sparkles: Create Quiz App

--- lib/udemy_adv_basics/main.dart
+++ lib/udemy_adv_basics/main.dart
@@ -1,26 +1,27 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/udemy_adv_basics/quiz_start.dart';
+// import 'package:flutter_lab/udemy_adv_basics/quiz_start.dart';
+import 'quiz.dart';
 
 void main() {
-  runApp(QuizApp());
+  runApp(const Quiz());
 }
-
-class QuizApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.deepPurple,
-          ),
-          child: Center(
-            child: QuizStart(),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//
+// class QuizApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//           decoration: BoxDecoration(
+//             color: Colors.deepPurple,
+//           ),
+//           child: Center(
+//             child: QuizStart(),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
